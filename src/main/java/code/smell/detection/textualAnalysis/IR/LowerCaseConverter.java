21631098c641370e89509100cb16f5fd350dc82d
Sharafat Ahmed Sabir <s.a.sabir@hotmail.com>
2017-11-11 17:53:18 +0600

    Lower case conversion done

--- src/main/java/code/smell/detection/textualAnalysis/IR/LowerCaseConverter.java
+++ src/main/java/code/smell/detection/textualAnalysis/IR/LowerCaseConverter.java
@@ -2,16 +2,31 @@ package code.smell.detection.textualAnalysis.IR;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
 @Component
 public class LowerCaseConverter {
 	public List<String> makeLowerCaseAllJavaFiles(List<String> javaFiles){
-		throw new java.lang.UnsupportedOperationException("Not supported yet.");
+		return makeListLowerCased(javaFiles);
 	}
 	
+	private List<String> makeListLowerCased(List<String> javaFiles) {
+		List<String> lowerCasedFiles = javaFiles.stream()
+                .map(String::toLowerCase)
+                .collect(Collectors.toList());
+		
+		return lowerCasedFiles;
+	}
+
 	public List<ArrayList<String>> makeLowerCaseAllMethods(List<ArrayList<String>> methods){
-		throw new java.lang.UnsupportedOperationException("Not supported yet.");
+		List<ArrayList<String>> lowerCaseMethods = new ArrayList<ArrayList<String>>();
+		
+		for(ArrayList<String> method : methods) {
+			lowerCaseMethods.add((ArrayList<String>) makeListLowerCased(method));
+		}
+		
+		return lowerCaseMethods;
 	}
 }
