d67a1dac08120a6c335e8e19140f5023eb24eb76
Sharafat Ahmed Sabir <s.a.sabir@hotmail.com>
2017-11-12 22:23:35 +0600

    Stemmer added

--- src/main/java/code/smell/detection/textualAnalysis/IR/Stemmer.java
+++ src/main/java/code/smell/detection/textualAnalysis/IR/Stemmer.java
@@ -7,11 +7,49 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Stemmer {
-	public List<String> stemAllJavaFiles(List<String> javaFiles){
-		throw new java.lang.UnsupportedOperationException("Not supported yet.");
-	}
 	
-	public List<ArrayList<String>> stemAllMethods(List<ArrayList<String>> methods){
-		throw new java.lang.UnsupportedOperationException("Not supported yet.");
+	public List<String> stemAllJavaFiles(List<String> javaFiles) {
+		List<String> wholeFiles = new ArrayList<>();
+		
+		for(String wholeClassString : javaFiles) {
+			String classFiles = "";
+			String[] splited = wholeClassString.trim().split("\\s+");
+			for(String singleWordInClass : splited) {
+				PottersStemmer pottersStemmer = new PottersStemmer();
+				pottersStemmer.add(singleWordInClass.toCharArray(), singleWordInClass.length());
+				
+				
+				classFiles += pottersStemmer.toString();
+			}
+			wholeFiles.add(classFiles);
+		}
+		
+		return wholeFiles;
 	}
+
+	public List<ArrayList<String>> stemAllMethods(List<ArrayList<String>> methods) {
+		List<ArrayList<String>> allMethodsOfFiles = new ArrayList<ArrayList<String>>();
+		
+		for(ArrayList<String> singleClass : methods) {
+			ArrayList<String> temporaryMethodList = new ArrayList<>();
+			for(String wholeClassString : singleClass) {
+				String classFiles = "";
+				String[] splited = wholeClassString.trim().split("\\s+");
+				for(String singleWordInClass : splited) {
+					PottersStemmer pottersStemmer = new PottersStemmer();
+					pottersStemmer.add(singleWordInClass.toCharArray(), singleWordInClass.length());
+					
+					
+					classFiles += pottersStemmer.toString();
+				}
+				temporaryMethodList.add(classFiles);
+			}
+			allMethodsOfFiles.add(temporaryMethodList);
+		}
+		
+		return allMethodsOfFiles;
+	}
+
+	
+
 }
